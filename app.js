58cb64c604e2a931094a3ae96e94a188a6b6723e
riswanmsm <riswan@tutorwizard.lk>
2019-02-25 04:55:16 +0530

    video 3 commit

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQLHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Welcome to graphql testing app with mongodb and express');
-});
+app.use('/api', graphQLHttp({
+    schema: buildSchema(`
+        type rootQuery{
+            events: [String!]!
+        }
+
+        type rootMutation{
+            createEvent(name: String): String
+        }
+
+        schema{
+            query: rootQuery
+            mutation: rootMutation
+        }    
+    `),
+    rootValue: {
+        events: () => {
+            return ["wedding", "Sports", "Press Meeting"];
+        },
+        createEvent: (args) => {
+            return args.name;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(8001, () => {
     console.log("Server started on 8001");
